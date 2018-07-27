9fdfd5317b2919815d4b512bcafbf03c529e7f16
RAJYALAKSHMI-PELLURI <41738406+RAJYALAKSHMI-PELLURI@users.noreply.github.com>
2018-07-27 16:10:10 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,20 @@
+class count
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+             int Number, Count=0;
+             Scanner sc=new Scanner(System.in);
+             sc = new Scanner(System.in);		
+             Number = sc.nextInt();
+             while(Number > 0) 
+             {
+            	Number = Number / 10;
+               	Count = Count + 1; 
+             }
+             System.out.println(Count);
+            }
+}
+
+
+        
