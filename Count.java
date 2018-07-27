169e62bb5ba0612d40e09142823bfbdeaac43bbd
RAJYALAKSHMI-PELLURI <41738406+RAJYALAKSHMI-PELLURI@users.noreply.github.com>
2018-07-27 16:17:03 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Count
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+             int Number, Count=0;
+             Scanner sc=new Scanner(System.in);
+             sc = new Scanner(System.in);		
+             Number = sc.nextInt();
+             while(Number > 0) 
+             {
+            	Number = Number / 10;
+               	Count = Count + 1; 
+             }
+             System.out.println(Count);
+            }
+}
+
