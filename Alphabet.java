a8cc2fd83945362e477f6cf38e91380128cf9d45
RAJYALAKSHMI-PELLURI <41738406+RAJYALAKSHMI-PELLURI@users.noreply.github.com>
2018-07-30 11:44:09 +0530

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -1,22 +1,18 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
-import java.util.Scanner;
 class Alphabets
 {
 	public static void main (String[] args) throws java.lang.Exception
     {
         char alphabet;
         Scanner sc= new Scanner(System.in);
-        System.out.print("Enter a Character : ");
         alphabet=sc.next().charAt(0);
         if((alphabet>='a' && alphabet<='z') || (alphabet>='A' && alphabet<='Z'))
         {
-            System.out.print(alphabet+"Alphabet");
+            System.out.print(alphabet+"/nAlphabet");
         }
         else
         {
-            System.out.print(alphabet+"No");
+            System.out.print(alphabet+"/nNo");
         }
     }
 }
